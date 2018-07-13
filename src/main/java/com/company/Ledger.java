c308a9d235503dc846bdb900d71faa2f156a4fe5
Boyan Kushlev <boyan.kushlev@paysafe.com>
2018-07-13 17:29:57 +0300

    create a gradle project structure

--- src/main/java/com/company/Ledger.java
+++ src/main/java/com/company/Ledger.java
@@ -3,21 +3,22 @@ package com.company;
 import java.security.NoSuchAlgorithmException;
 
 public class Ledger {
+
     private static final int LEDGER_SIZE = 100;
     private Transaction[] transactions = new Transaction[LEDGER_SIZE];
     private int counter = 0;
 
-    public void addTransaction(Transaction  trans) {
-        if(counter < LEDGER_SIZE) {
+    public void addTransaction(Transaction trans) {
+        if (counter < LEDGER_SIZE) {
             transactions[counter] = trans;
             counter++;
         }
     }
 
     public String hash() throws NoSuchAlgorithmException {
-        String hash= "";
-        for(Transaction t: transactions){
-            hash = Hasher.hash(hash+t.hash());
+        String hash = "";
+        for (Transaction t : transactions) {
+            hash = Hasher.hash(hash + t.hash());
         }
         return hash;
     }
