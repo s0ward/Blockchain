bfc7df35ea4488a74cee93084180d8fcfbeb19c0
s0ward <kristiyantomov@gmail.com>
2018-07-19 13:15:10 +0300

    hashIsValid in Miner

--- src/main/java/com/company/Block.java
+++ src/main/java/com/company/Block.java
@@ -7,22 +7,27 @@ public class Block {
 
     private static int blockCounter=0;
     private int blockNumber = 0;
+    private BlockHeader header;
     private String headerHash;
     private Ledger ledger;
     private int blockSize;
 
 
-
-    public Block(){
+    public Block(BlockHeader header, Ledger ledger) throws NoSuchAlgorithmException {
         blockCounter++;
         blockNumber = blockCounter;
+        this.header = header;
+        this.ledger = ledger;
+        this.headerHash = header.hash();
         System.out.println("Block number: "+this.blockNumber+" is created");
     }
 
+    public int getBlockNumber(){
+        return this.blockNumber;
+    }
 
-//    private String hash() throws NoSuchAlgorithmException {
-//        return Hasher.hash(new Integer(blockNumber)+prevHash+timestamp.toString()+
-//                nonce+ledger.hash()+new Integer(blockSize));
-//    }
+    private String hash() throws NoSuchAlgorithmException {
+        return Hasher.hash(this.toString());
+    }
 
 }
--- src/main/java/com/company/BlockHeader.java
+++ src/main/java/com/company/BlockHeader.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 public class BlockHeader {
@@ -9,5 +10,25 @@ public class BlockHeader {
     private String nonce;
     private String ledgerHash;
 
+    public BlockHeader(String prevHash, String nonce, String ledgerHash) {
+        this.prevHash = prevHash;
+        this.nonce = nonce;
+        this.ledgerHash = ledgerHash;
+        this.timestamp = new Date();
+    }
 
+    @Override
+    public String toString() {
+
+        return "BlockHeader{" +
+            "prevHash='" + prevHash + '\'' +
+            ", timestamp=" + timestamp +
+            ", nonce='" + nonce + '\'' +
+            ", ledgerHash='" + ledgerHash + '\'' +
+            '}';
+    }
+
+    public String hash() throws NoSuchAlgorithmException {
+        return Hasher.hash(this.toString());
+    }
 }
