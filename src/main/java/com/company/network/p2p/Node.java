fe8f5422b25e504a6bbd06141e7eb0c6ce3c0d62
s0ward <kristiyantomov@gmail.com>
2018-07-22 13:18:06 +0300

    NodeServerThread added

--- src/main/java/com/company/network/p2p/Node.java
+++ src/main/java/com/company/network/p2p/Node.java
@@ -1,9 +1,6 @@
 package com.company.network.p2p;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,21 +11,15 @@ public class Node {
     private static final int MAX_NEIGHBORS = 3;
     private String inetaddr;
     private ArrayList<String> neighbors = new ArrayList<>(MAX_NEIGHBORS);
-    private ServerSocket serverSocket;
 
     public Node() throws Exception {
         this.inetaddr = getIp();
-        serverSocket = new ServerSocket(LISTENING_PORT);
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(LISTENING_PORT);
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            NodeClientThread nodeClientThread = new NodeClientThread(socket);
-            nodeClientThread.start();
-        }
+            NodeServerThread nodeServerThread = new NodeServerThread();
+            nodeServerThread.start();
 
     }
 
@@ -51,7 +42,29 @@ public class Node {
         }
     }
 
+    private void sendMessage(String host, String message){
+        Socket socket = null;
+        try {
+            socket = new Socket(host, LISTENING_PORT);
+            BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            socketWriter.write(message);
+            socketWriter.flush();
+
+            socketWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessageToAll(String message) {
+        for (String neighbor : neighbors) {
+            sendMessage(neighbor, message);
+        }
+    }
+
     public String getInetAddr() {
         return inetaddr;
     }
+
 }
